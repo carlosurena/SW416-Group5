633a0d9aee843c958c7581acdfe5cda0f0381d86
Carlos Urena <carlos.urena@student.fairfield.edu>
2018-03-20 22:46:34 -0400

    initial commit

---
+++ FitnessApp/FitnessApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FitnessApp
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
