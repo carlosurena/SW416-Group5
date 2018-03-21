e5e4445db73906128997bb7bff1b4162df9ea01b
Carlos Urena <carlos.urena@student.fairfield.edu>
2018-03-21 00:49:24 -0400

    Me and Cardio Page templates

---
+++ FitnessApp/FitnessApp/ViewModels/MeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace FitnessApp
+{
+    public class CardioViewModel : BaseViewModel
+    {
+        public CardioViewModel()
+        {
+            Title = "Cardio Workouts";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
