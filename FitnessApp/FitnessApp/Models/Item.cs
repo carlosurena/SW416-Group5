13ac0f0ff09b016d1eb90909f7c67f23a9c390d4
Carlos Urena <carlos.urena@student.fairfield.edu>
2018-03-21 20:25:51 -0400

    added "Me" and "Featured" menu options

--- FitnessApp/FitnessApp/Models/Item.cs
+++ FitnessApp/FitnessApp/Models/Item.cs
@@ -7,5 +7,6 @@ namespace FitnessApp
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+        public string Day { get; set;   }
     }
 }
