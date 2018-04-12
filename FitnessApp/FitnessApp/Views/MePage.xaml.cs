917bfbca1a2a232e0eb17190bdefa777f9f12d27
skurtz00 <35576480+skurtz00@users.noreply.github.com>
2018-04-11 22:10:34 -0400

    working update

--- FitnessApp/FitnessApp/Views/MePage.xaml.cs
+++ FitnessApp/FitnessApp/Views/MePage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
@@ -7,9 +7,28 @@ namespace FitnessApp
 {
     public partial class MePage : ContentPage
     {
+        MeViewModel viewModel;
         public MePage()
         {
             InitializeComponent();
+            BindingContext = viewModel = new MeViewModel();
+        }
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewItemPage());
+        }
+
+        async void Me_Clicked(object sender, EventArgs e)
+        {
+            ItemList.IsVisible = true;
+            FeaturedPage.IsVisible = false;
+
+        }
+        async void Featured_Clicked(object sender, EventArgs e)
+        {
+            FeaturedPage.IsVisible = true;
+            ItemList.IsVisible = false;
+
         }
     }
 }
