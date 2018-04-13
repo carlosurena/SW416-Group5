07baf50db196706d5f00285c8f7e88826f7e7f1b
Xin Ning <jxnx888@hotmail.com>
2018-04-14 04:04:08 +0800

    button color change

--- FitnessApp/FitnessApp/Views/CardioPage.xaml.cs
+++ FitnessApp/FitnessApp/Views/CardioPage.xaml.cs
@@ -29,21 +29,27 @@ namespace FitnessApp
             RunPage.IsVisible = true;
             WalkPage.IsVisible = false;
             BikePage.IsVisible = false;
-
+            BtnRun.BackgroundColor = Color.LightGreen;
+            BtnWalk.BackgroundColor = Color.DeepSkyBlue;
+            BtnBike.BackgroundColor = Color.DeepSkyBlue;
         }
         async void BtnWalkClicked(object sender, EventArgs e)
         {
             RunPage.IsVisible = false;
             WalkPage.IsVisible = true;
             BikePage.IsVisible = false;
-
+            BtnRun.BackgroundColor = Color.DeepSkyBlue;
+            BtnWalk.BackgroundColor = Color.LightGreen;
+            BtnBike.BackgroundColor = Color.DeepSkyBlue;
         }
         async void BtnBikeClicked(object sender, EventArgs e)
         {
             RunPage.IsVisible = false;
             WalkPage.IsVisible = false;
             BikePage.IsVisible = true;
-
+            BtnRun.BackgroundColor = Color.DeepSkyBlue;
+            BtnWalk.BackgroundColor = Color.DeepSkyBlue;
+            BtnBike.BackgroundColor = Color.LightGreen;
         }
 
     }
