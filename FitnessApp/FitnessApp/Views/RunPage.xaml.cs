2d58b36eb8ed736c04e5cfc8384e9fd69b7123c8
Xin Ning <jxnx888@hotmail.com>
2018-04-14 02:58:43 +0800

    CardioPages

---
+++ FitnessApp/FitnessApp/Views/RunPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FitnessApp
+{
+    public partial class RunPage : StackLayout
+    {
+        public RunPage()
+        {
+            InitializeComponent();
+        }
+
+
+        async void BtnGoClicked(object sender, EventArgs e)
+        {
+        await Navigation.PushAsync(new MapPage());
+        }
+        //  async void OnGoButtonClicked(object sender, EventArgs args)
+        //{
+        //  DisplayAlert("Alert", "Ready to Go?", "Yes", "No");  Clicked="OnGoButtonClicked" 
+        //}
+
+    }
+}
