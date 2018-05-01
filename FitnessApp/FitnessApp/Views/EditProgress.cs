6be5be63fbbffdc4040ceee0583967b88fabaeb4
skurtz00 <35576480+skurtz00@users.noreply.github.com>
2018-04-30 22:59:43 -0400

    working edit page

--- FitnessApp/FitnessApp/Views/EditProgress.cs
+++ FitnessApp/FitnessApp/Views/EditProgress.cs
@@ -1,20 +1,34 @@
-﻿using System;
-
+using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
-namespace FitnessApp.Views
+namespace FitnessApp
 {
-    public class EditProgress : ContentPage
+    public partial class EditProgress : ContentPage
     {
         public EditProgress()
         {
-            Content = new StackLayout
+            InitializeComponent();
+        }
+        void Slider_ValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            if (sender == run_slider)
             {
-                Children = {
-                    new Label { Text = "Hello ContentPage" }
-                }
-            };
+                runLabel.Text = String.Format("Miles to Run = {0:N1}", (int)args.NewValue);
+            }
+            else if (sender == bike_slider)
+            {
+                bikeLabel.Text = String.Format("Miles to Bike = {0:N1}", (int)args.NewValue);
+            }
+            else if (sender == walk_slider)
+            {
+                walkLabel.Text = String.Format("Miles to Walk = {0:N1}", (int)args.NewValue);
+            }
+        }
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+          // MessagingCenter.Send(this, "SaveGoal");
+          //  await Navigation.PopToRootAsync();
         }
     }
 }
-
