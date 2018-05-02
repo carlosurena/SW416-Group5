ad2e354639fc099b563ae68387260f23b6e8bbe9
Sarah <sarah.kurtz@student.fairfield.edu>
2018-05-02 16:49:08 -0400

    password page

--- FitnessApp/FitnessApp/Views/MePage.xaml.cs
+++ FitnessApp/FitnessApp/Views/MePage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace FitnessApp
@@ -22,11 +26,59 @@ namespace FitnessApp
         }
         async void Protected_Clicked(object sender, EventArgs e)
         {
+
+            var result = await OpenPasswordPage();
             ProgressPage.IsVisible = false;
-            await DisplayAlert ("Password", "Enter Password", "Submit", "Cancel");
             ProtectedPage.IsVisible = true;
 
         }
+
+        private async Task<string> OpenPasswordPage()
+        {
+            // create the TextInputView
+            var inputView = new PasswordPage(
+                "Enter Password", "Password", "Submit", "Cancel", "Invalid Password");
+
+            // create the Transparent Popup Page
+            // of type string since we need a string return
+            var popup = new InputAlertDialogBase<string>(inputView);
+
+            // subscribe to the TextInputView's Button click event
+            inputView.SaveButtonEventHandler +=
+                (sender, obj) =>
+                {
+                    if (!string.IsNullOrEmpty(((PasswordPage)sender).TextInputResult))
+                    {
+                        ((PasswordPage)sender).IsValidationLabelVisible = false;
+                        popup.PageClosedTaskCompletionSource.SetResult(((PasswordPage)sender).TextInputResult);
+                    }
+                    else
+                    {
+                        ((PasswordPage)sender).IsValidationLabelVisible = true;
+                    }
+                };
+
+            // subscribe to the TextInputView's Button click event
+            inputView.CancelButtonEventHandler +=
+                (sender, obj) =>
+                {
+                    popup.PageClosedTaskCompletionSource.SetResult(null);
+                };
+
+            // Push the page to Navigation Stack
+            await PopupNavigation.PushAsync(popup);
+
+            // await for the user to enter the text input
+            var result = await popup.PageClosedTask;
+
+            // Pop the page from Navigation Stack
+            await PopupNavigation.PopAsync();
+
+            // return user inserted text value
+            return result;
+        }
+
+
         async void About_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new AboutPage());
---
+++ FitnessApp/FitnessApp/Views/Password_Page.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FitnessApp.Views
+{
+    public partial class Password_Page : ContentView
+    {
+        public Password_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
